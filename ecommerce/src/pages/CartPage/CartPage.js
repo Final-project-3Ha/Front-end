fc41e354c9330cee9f66dbeec5fa393338021763
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-05-25 17:35:05 +0300

    Finished CartItems

--- ecommerce/src/pages/CartPage/CartPage.js
+++ ecommerce/src/pages/CartPage/CartPage.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col, Alert, ListGroup, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import CartItemComponent from "../../components/CartItem/CartItemComponent.js";
 
 function CartPage() {
   return (
-    <div>
-      This is a CartPage
-    </div>
-  )
+    <Container>
+      <Row className="mt-4">
+        <Col md={8}>
+          <h1>Shopping Cart</h1>
+          <ListGroup variant="flush">
+            {Array.from({ length: 3 }).map((item, idx) => (
+              <CartItemComponent key={idx} />
+            ))}
+          </ListGroup>
+          <Alert variant="info">Your cart is empty</Alert>
+        </Col>
+        <Col md={4}>
+          <ListGroup>
+            <ListGroup.Item>
+              <h3>Subtotal (2 Items)</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price: <span className="fw-bold">$200</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <LinkContainer to="/user/order-details">
+                <Button type="button">Proceed To Checkout</Button>
+              </LinkContainer>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
-export default CartPage
+export default CartPage;
