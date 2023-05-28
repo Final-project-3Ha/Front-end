b2205b41e4e7b37ca4cd72a14d96fcba66b428ea
Hassan Hammoud <hassanhammoud2000@gmail.com>
2023-05-28 21:11:52 +0300

    Add Extension for Redux in FireFox

--- ecommerce/src/redux/store.js
+++ ecommerce/src/redux/store.js
@@ -1,22 +1,21 @@
-import {createStore} from 'redux';
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const counterReducer = (state = {value:0}, action) => {
-   switch (action.type){
-    case 'ADD': 
-    return {value: state.value + 1 + action.someValue};
-    default: 
-    return state;
-   }
+const counterReducer = (state = { value: 0 }, action) => {
+  switch (action.type) {
+    case "ADD":
+      return { value: state.value + 1 + action.someValue };
+    default:
+      return state;
+  }
+};
 
-}
- 
-const store = createStore(counterReducer, {value:0});
+
+const store = createStore(counterReducer, { value: 0 }, composeWithDevTools());
 
 store.dispatch({
-    type: 'ADD',
-    someValue: 10
-})
-
-console.log(store.getState())
+  type: "ADD",
+  someValue: 10,
+});
 
 export default store;
